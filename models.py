7bbe2ed35080b26210b5781eb5159d05eb1bb6ec
Matheus <mrs.matheusreis@gmail.com>
2021-05-05 22:45:35 -0300

    Adicionei endpoints diretores
    adicionei endpoints dos diretores, e começando a implementar os updates
    e deletes

--- models.py
+++ models.py
@@ -1,7 +1,16 @@
-from main import insert,select
+from cmd_bd import insert,select,update,delete
 
 def insert_genero(nome):
     insert("generos",["nome"],[nome])
 
 def get_genero(nome):
-    select("locadora.generos","nome",nome)
+    select("generos","nome",nome)
+
+def insert_diretor(nome_completo):
+    insert("diretores",["nome_completo"],[nome_completo])
+
+def get_diretor(nome_completo):
+    select("diretores","nome_completo",nome_completo)
+
+def update_diretor(id,valor_chave,nome_completo):
+    update("diretores",["id"],["valor_chave"],"nome_completo",["valores"],[id,valor_chave,nome_completo])
--- serializers.py
+++ serializers.py
@@ -8,3 +8,15 @@ def genero_from_db(*args):
     return {
         "nome": args[0]
     }
+
+
+def diretores_from_web(**kwargs):
+    return {
+        "nome_completo": kwargs["nome_completo"] if "nome_completo" in kwargs else ""
+    }
+
+
+def diretores_from_db(*args):
+    return {
+        "nome_completo": args[0]
+    }
